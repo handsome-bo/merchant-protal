81b8f9fa9adb38ef9e10b490098e826b37189775
backhideker <1243974920@qq.com>
2021-04-19 15:00:48 +0800

    2021/4/19

---
+++ src/views/errorPage.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="outer">
+        <div class="group">
+           <p>
+            <i class="el-icon-warning">    
+            </i>
+           </p>
+         <div class="text">
+             {{$t('errorpage.errormessage')}}
+             </div>   
+      
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+    name:'ErrorPage',
+}
+</script>>
+
+<style scoped>
+@import url("//unpkg.com/element-ui@2.15.1/lib/theme-chalk/index.css");
+   .group {
+  margin: 0 auto;
+  width: 500px;
+  padding: 50px;
+  color: #ffffff;
+  font-family: "Microsoft JhengHei";
+}
+p{
+       
+    text-align: center;
+}
+.el-icon-warning {
+    font-size: 50px;
+ 
+ 
+ 
+}
+.text {
+
+  font-size: 30px;
+  font-weight: normal;
+  letter-spacing: 0;
+  line-height: 40px;
+  text-align: center;
+  text-shadow: 0 2px 10px 0 rgba(0, 0, 0, 0.9);
+}
+</style>
