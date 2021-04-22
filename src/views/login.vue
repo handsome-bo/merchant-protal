a893d242767907d22dfbbdecab58ea4a31040373
Zimo-first <2130765871@qq.com>
2021-04-22 16:45:40 +0800

    commit

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,36 +1,17 @@
 <template>
   <div class="outer">
     <div class="group">
-      <div class="title">{{ $t("login.name") }}</div>
-      <div class="title1">{{ $t("login.registeredemailaddress") }}</div>
-      <div>
-        <input
-          class="input"
-          :placeholder="$t('login.emailaddress')"
-          v-model="email"
-        />
-      </div>
-      <div>
-        <input
-          class="input"
-          type="password"
-          :placeholder="$t('login.password')"
-          v-model="password"
-        />
-      </div>
+      <div class="title">{{$t("login.name")}}</div>
+      <div class="title1">{{$t("login.registeredemailaddress")}}</div>
+      <div><input class="input" :placeholder="$t('login.emailaddress')" v-model="email"/></div>
+      <div><input class="input" type="password" :placeholder="$t('login.password')" v-model="password"/></div>
 
       <div class="other-text flex align-center space-between">
-        <div class="pointer" @click="forget()">
-          {{ $t("login.forgotpassword") }}
-        </div>
-        <div>
-          <input class="checkbox" type="checkbox" />{{ $t("login.keepsignin") }}
-        </div>
+        <div>{{$t("login.forgotpassword")}}</div>
+        <div><input class="checkbox" type="checkbox" />{{$t("login.keepsignin")}}</div>
       </div>
       <div class="text-center">
-        <el-button class="login-btn" @click="login">{{
-          $t("login.signin")
-        }}</el-button>
+        <el-button class="login-btn" @click="login">{{$t("login.signin")}}</el-button>
       </div>
     </div>
   </div>
@@ -38,139 +19,104 @@
 
 <script>
 export default {
-  data() {
-    return {
-      email: "",
-      password: "",
-    };
+  name: "Login",
+date(){
+ return{
+   email:'',
+   password:'',
+ }
   },
   methods: {
     login() {
       this.$router.push("/main");
       return;
-      if (
-        !/^\w+([\.\-]\w+)*\@\w+([\.\-]\w+)*\.\w+$/.test(this.email) ||
-        this.password == null
-      ) {
-        this.alertMessage();
-      } else {
-        this.$axios({
-          url: "https://localhost:44316/Settings/LoginUserInfo",
-          method: "POST",
-        }).then((res) => {
-          if (res && res.status == "200" && res.data) {
-            console.log(res);
-            var data = res.data;
-            const userinfo = {
-              name: data.name,
-              firstname: data.firstname,
-              firstnametc: data.firstnametc,
-              lastname: data.lastname,
-              lastnametc: data.lastnametc,
-              email: data.email,
-              salutation: data.salutation,
-              salutationtc: data.salutationtc,
-              title: data.title,
-              titletc: data.titletc,
-              mobilenumber: data.mobilenumber,
-              canacknowledgeevouchersummaryreport:
-                data.canacknowledgeevouchersummaryreport,
-              receivemonthlyevouchersummaryreport:
-                data.receivemonthlyevouchersummaryreport,
-              receivedailyevouchersummaryreport:
-                data.receivedailyevouchersummaryreport,
-              contacttype: data.contacttype,
-            };
-            this.$store.commit("setUserInfo", userinfo);
-            this.$store.commit("setToken", "123213123");
-            this.$router.push("/main");
-          }
+      if(!/^\w+([\.\-]\w+)*\@\w+([\.\-]\w+)*\.\w+$/.test(this.email)
+      ||this.password==null){
+         this.alertMessage();
+      }
+      else{
+         this.$router.push("/main");
+      }
+    
+    },
+    alertMessage(){
+         this.$message({
+          message: '邮箱或密码输入有误，请重新登录',
+          type: 'warning'
         });
-        }
-    },
-    forget() {
-      this.$router.push("/forgotpassword1");
-    },
-    alertMessage() {
-      this.$message({
-        message: "邮箱或密码输入有误，请重新登录",
-        type: "error",
-      });
-      // callback: action => {
-      //   this.$message({
-      //     type: 'info',
-
-      //   });
-      // }
-    },
+          // callback: action => {
+          //   this.$message({
+          //     type: 'info',
+              
+          //   });
+          // }
+        
+    }
   },
 };
 </script>
 
 <style   scoped>
-  @media (max-width: 768px) {
-    .title {
+@media (max-width: 768px){
+  .group{
+    height: 341px;
+    width: 305px;
+  }
+  .input{
+  width: 305px;
+  }
+  .title{
   height: 32px;
-  width: 250px;
-  color: #ffffff;
-  font-size: 24px;
-  line-height: 32px;
-}
-
-.title1 {
-  height: 40px;
-  width: 250px;
   color: #FFFFFF;
   font-family: "Microsoft JhengHei";
-  font-size: 16px;
-  line-height: 21px;
-  text-align: center;
-  text-shadow: 0 2px 10px 0 rgba(0,0,0,0.8);
-}
-
-.input {
-  
-  width: 305px;
-  
-}
-
-  }
-
-  @media (min-width: 768px){
-    .input{
-       width: 500px;
-    }
-  }
-
-.title {
-  font-size: 30px;
+  font-size: 24px;
   font-weight: bold;
   letter-spacing: 0;
-  line-height: 40px;
-  text-align: center;
-  text-shadow: 0 2px 10px 0 rgba(0, 0, 0, 0.9);
+  line-height: 32px;
+  }
+  .title1{
+    margin-top: 42px;
+  }
 }
 
-.title1 {
-  font-size: 20px;
-  letter-spacing: 0;
-  line-height: 27px;
-  margin-top: 60px;
+@media (min-width: 768px){
+.group{
+   width: 500px;
+}
+.input{
+    width: 500px;
+}
+.title{
+  font-size: 30px;
+  font-weight: bold;
+  line-height: 40px;
+}
+.title1{
+margin-top: 60px;
+}
 }
 
 .group {
   margin: 0 auto;
-  width: 500px;
-  height: 341px;
-  width: 305px;
-
+  padding: 50px;
   color: #ffffff;
   font-family: "Microsoft JhengHei";
+}
+
+.title { 
+  letter-spacing: 0;
+  text-align: center;
+  text-shadow: 0 2px 10px 0 rgba(0, 0, 0, 0.9);
+}
+.title1 {
+  font-size: 20px;
+  letter-spacing: 0;
+  line-height: 27px;
+  
 }
 .input {
   box-sizing: border-box;
   height: 50px;
- 
   border: 1px solid rgba(0, 0, 0, 0.4);
   border-radius: 10px;
   background-color: #ffffff;
